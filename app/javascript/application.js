6ea72083cdb06614e91106dc643f8021f297d999
SimSimSim <leivaprogramming@gmail.com>
2023-10-19 16:21:48 -0400

    add bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// bootstrap
+import * as bootstrap from "bootstrap"
+window.Bootstrap = bootstrap; // Necessary for popper.js

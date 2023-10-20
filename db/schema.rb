81b8cf9e765cac83fe2a84bad03bcb93d2ef1dbb
SimSimSim <leivaprogramming@gmail.com>
2023-10-20 11:49:35 -0400

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_205331) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_150034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_205331) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

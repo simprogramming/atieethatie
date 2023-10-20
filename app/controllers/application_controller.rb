63854788ba70e53e08ca14b11eb1e671678cf3e7
SimSimSim <leivaprogramming@gmail.com>
2023-10-20 09:34:22 -0400

    cleaning some code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include NoticeHelper
   include LocaleHelper
+
+  layout :resolve_layout
+
+  def resolve_layout
+    "application"
+  end
 end

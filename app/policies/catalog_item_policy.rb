046f462101026c447d6e7171704bcd824ffe8a8a
SimSimSim <leivaprogramming@gmail.com>
2023-11-11 11:25:25 -0500

    update params bug and image bug

--- app/policies/catalog_item_policy.rb
+++ app/policies/catalog_item_policy.rb
@@ -7,6 +7,7 @@ class CatalogItemPolicy < ApplicationPolicy
 
   def permitted_attributes
     [:name_fr, :name_en, :description_fr, :description_en, :square_id, :version, :category_id, :fragrance_id,
-     { catalog_item_variations_attributes: %i[name_fr name_en inventory price color size], images: [] }]
+     { catalog_item_variations_attributes: %i[name_fr name_en price color size information_fr information_en
+                                              instruction_fr instruction_en], images: [] }]
   end
 end
--- app/policies/catalog_item_variation_policy.rb
+++ app/policies/catalog_item_variation_policy.rb
@@ -7,6 +7,6 @@ class CatalogItemVariationPolicy < ApplicationPolicy
 
   def permitted_attributes
     [:name_fr, :name_en, :information_fr, :information_en, :square_id, :version, :catalog_item_id, :price, :size,
-     :color, :sku, :image_url, :inventory, {images: []}]
+     :instruction_fr, :instruction_en, :color, :sku, :image_url, {images: []}]
   end
 end

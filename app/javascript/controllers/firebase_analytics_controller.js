3a64191d33e6b13c578bacecf67c68e106980e8f
SimSimSim <digitalmenustudio@gmail.com>
2024-06-14 13:18:01 -0400

    add analytics

---
+++ app/javascript/controllers/firebase_analytics_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+export default class extends Controller {
+  connect() {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }
+}

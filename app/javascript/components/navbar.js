d17e8b0b1b148e7a04229301ae0b4a06287f0ea0
SimSimSim <digitalmenustudio@gmail.com>
2024-06-14 01:30:49 -0400

    add banner shipping

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,8 +4,11 @@ document.addEventListener("turbo:load", function() {
         if (navbar) {
             if (window.scrollY >= window.innerHeight*.05) {
                 navbar.classList.add('navbar-white');
+                navbar.classList.remove("py-5")
             } else {
                 navbar.classList.remove('navbar-white');
+                navbar.classList.add('py-5');
+
             }
         }
     });
